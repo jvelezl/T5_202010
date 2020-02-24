6fd49a79f417652b616b7c8659ac8fa16cf935c4
Juan David <jcardonapaez@gmail.com>
2020-02-24 16:42:57 -0500

    Test

--- test/test/logic/TestModelo.java
+++ test/test/logic/TestModelo.java
@@ -6,20 +6,20 @@ import model.logic.Modelo;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.*; 
+import model.logic.Modelo;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class TestModelo {
 	
 	private Modelo modelo;
-	private static int CAPACIDAD=100;
 	
 	@Before
 	public void setUp1() {
-		modelo= new Modelo(CAPACIDAD);
-	}
-
-	public void setUp2() {
-		for(int i =0; i< CAPACIDAD;i++){
-			modelo.agregar(""+i);
-		}
+		modelo= new Modelo();
+		modelo.cargarDatos();
 	}
 
 	@Test
@@ -30,25 +30,17 @@ public class TestModelo {
 
 	@Test
 	public void testDarTamano() {
-		// TODO
-	}
-
-	@Test
-	public void testAgregar() {
-		// TODO Completar la prueba
+		setUp1();
+		assertSame(19,modelo.darTamano());
 	}
 
 	@Test
 	public void testBuscar() {
-		setUp2();
+		setUp1();
+		assertSame(176695, modelo.buscarPorId(176695).darId());
 		// TODO Completar la prueba
 	}
 
-	@Test
-	public void testEliminar() {
-		setUp2();
-		// TODO Completar la prueba
-		
-	}
+
 
 }
